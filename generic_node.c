7bb73ff223320d8b68faff14a0cba469ed0983fd
Arson Marianus <33475446+m-arson@users.noreply.github.com>
2024-11-03 00:17:26 +0700

    Update generic_node.c

--- generic_node.c
+++ generic_node.c
@@ -34,21 +34,55 @@ int main(void) {
 		exit(EXIT_FAILURE);
 	}
 
-	User user1;
+	User user1, user2;
 	snprintf(user1.firstName, sizeof(user1.firstName), "Arson");
 	snprintf(user1.lastName, sizeof(user1.lastName), "Marianus");
 	user1.age = 26;
 
 	memcpy(head->data, &user1, sizeof(User));
+	head->next = NULL;
+
+	current = (Node*) malloc(sizeof(Node));
+	if (current == NULL) {
+		free(head);
+		free(head->data);
+		perror("Cannot allocate more memory for Node");
+		exit(EXIT_FAILURE);
+	}
+
+	current->data = malloc(sizeof(User));
+	if (current->data == NULL) {
+		free(head);
+		free(head->data);
+		free(current);
+		perror("Cannot allocate more memory for User Data");
+		exit(EXIT_FAILURE);
+	}
+
+	snprintf(user2.firstName, sizeof(user2.firstName), "Koila");
+	snprintf(user2.lastName, sizeof(user2.lastName), "Jeniffer");
+	user2.age = 30;
+
+	memcpy(current->data, &user2, sizeof(User));
+	current->next = NULL;
+
 	head->next = current;
 
-	User *userData1 = (User*) head->data;
+	User *userdata1 = (User*) head->data;
+	User *userdata2 = (User*) head->next->data;
 
-	printf("First Name : %s\n", userData1->firstName);
-	printf("Last Name  : %s\n", userData1->lastName);
-	printf("Age        : %d\n", userData1->age);
+	printf("User 1 :\n");
+	printf("FirstName : %s\n", userdata1->firstName);
+	printf("LastName  : %s\n", userdata1->lastName);
+	printf("Age       : %d\n", userdata1->age);
 
+	printf("\nUser 2 :\n");
+	printf("FirstName : %s\n", userdata2->firstName);
+	printf("LastName  : %s\n", userdata2->lastName);
+	printf("Age       : %d\n", userdata2->age);
 
+	free(current->data);
+	free(current);
 	free(head->data);
 	free(head);
 
